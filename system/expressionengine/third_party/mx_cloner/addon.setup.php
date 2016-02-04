5cd3eb96162060ea2c59d7134d3cdbfae21524ab
Cesar Gomez <cesargomez89@gmail.com>
2016-02-04 17:55:04 -0600

    fixing for 3.0

---
+++ system/expressionengine/third_party/mx_cloner/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'author'      => 'Cesar Gomez',
+  'author_url'  => 'https://github.com/cesargomez89/mx_cloner',
+  'name'        => 'mx_cloner',
+  'description' => 'Clone EE entries',
+  'version'     => '1.3.0',
+  'namespace'   => 'accesory\mx_cloner'
+);
